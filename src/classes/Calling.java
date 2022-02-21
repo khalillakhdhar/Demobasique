fce069cfc43d9a1c9d4191ff234d2c9913ce5a78
khalil <khalillakhdharatc@gmail.com>
2022-02-21 09:59:30 +0100

    code

---
+++ src/classes/Calling.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Calling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Voiture v1=new Voiture();
+		Voiture v2=new Voiture("teste", 10000, "red", "teste");
+		
+	}
+
+}
---
+++ src/classes/Voiture.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class Voiture {
+String marque;
+double prix;
+String couleur,carburant;
+public Voiture(String marque, double prix, String couleur, String carburant) {
+	
+	this.marque = marque;
+	this.prix = prix;
+	this.couleur = couleur;
+	this.carburant = carburant;
+}
+public Voiture() {
+	// TODO Auto-generated constructor stub
+}
+
+
+
+
+}
+
